285deae32a5c2a0862d94ee643cd543a3ae5425c
Becky Phan <beckyphan@live.com>
2021-02-18 05:41:07 -0800

    baby phan-wheeler is born!

--- src/components/Baby.js
+++ src/components/Baby.js
@@ -6,10 +6,10 @@ function Baby() {
     <div className="App">
       <header className="App-header">
         <h1>
-          baby phan-wheeler
+          Hayden J. Phan-Wheeler
         </h1>
         <p>
-          <Link to="/birthday" className="Hlink"><button className="soon">Coming Soon</button></Link>
+          <Link to="/birthday" className="Hlink"><button className="soon">has arrived!</button></Link>
         </p>
       </header>
     </div>
--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -7,32 +7,32 @@ class Birthday extends React.Component {
 
   render() {
     let   guesses = [
-        { name: "Brandon", month: 2, day: 11 },
-        { name: "Becky", month: 2, day: 12},
-        { name: "Allan", month: 2, day: 22},
-        { name: "Annie", month: 2, day: 17},
-        { name: "Andrew", month: 2, day: 16},
-        { name: "Sarah", month: 2, day: 18},
-        { name: "Dan", month: 2, day: 20},
-        { name: "Grace", month: 2, day: 14},
-        { name: "Mia", month: 2, day: 14},
-        { name: "Nita", month: 2, day: 5},
-        { name: "David", month: 2, day: 21},
-        { name: "Jordyn", month: 2, day: 15},
-        { name: "Jessica", month: 2, day: 11},
-        { name: "Shap", month: 2, day: 13},
-        { name: "Ben", month: 2, day: 14},
-        { name: "Peter", month: 2, day: 12},
-        { name: "Marshall", month: 2, day: 10},
-        { name: "Jamie", month: 2, day: 12},
-        { name: "Jennie", month: 2, day: 14},
-        { name: "Ari", month: 2, day: 12},
-        { name: "Zoe", month: 2, day: 18},
-        { name: "Colleen", month: 2, day: 16},
-        { name: "Karen", month: 2, day: 19},
-        { name: "Nita's #2 Guess", month: 2, day: 8},
-        { name: "Nita's #3 Guess", month: 2, day: 11},
-        { name: "Tiffle", month: 2, day: 19}
+        {status: "out", name: "Brandon", month: 2, day: 11 },
+        {status: "out", name: "Becky", month: 2, day: 12},
+        {status: "out", name: "Allan", month: 2, day: 22},
+        {status: "out", name: "Annie", month: 2, day: 17},
+        {status: "out", name: "Andrew", month: 2, day: 16},
+        {status: "out", name: "Sarah", month: 2, day: 18},
+        {status: "out", name: "Dan", month: 2, day: 20},
+        {status: "out", name: "Grace", month: 2, day: 14},
+        {status: "out", name: "Mia", month: 2, day: 14},
+        {status: "out", name: "Nita", month: 2, day: 5},
+        {status: "out", name: "David", month: 2, day: 21},
+        {status: "out", name: "Jordyn", month: 2, day: 15},
+        {status: "out", name: "Jessica", month: 2, day: 11},
+        {status: "out", name: "Shap", month: 2, day: 13},
+        {status: "out", name: "Ben", month: 2, day: 14},
+        {status: "out", name: "Peter", month: 2, day: 12},
+        {status: "out", name: "Marshall", month: 2, day: 10},
+        {status: "out", name: "Jamie", month: 2, day: 12},
+        {status: "out", name: "Jennie", month: 2, day: 14},
+        {status: "out", name: "Ari", month: 2, day: 12},
+        {status: "in", name: "Zoe", month: 2, day: 18},
+        {status: "in", name: "Colleen", month: 2, day: 16},
+        {status: "out", name: "Karen", month: 2, day: 19},
+        {status: "out", name: "Nita's #2 Guess", month: 2, day: 8},
+        {status: "out", name: "Nita's #3 Guess", month: 2, day: 11},
+        {status: "out", name: "Tiffle", month: 2, day: 19}
       ]
 
     let sortedGuesses = guesses.sort((a, b) => a.day > b.day ? 1:-1)
@@ -44,7 +44,7 @@ class Birthday extends React.Component {
         <NavBar />
         <h1>Birthday Guesses</h1>
         <div className="Guesses">
-          {sortedGuesses.map((guess, i) => <Guess key={i} name={guess.name} month={guess.month} day={guess.day} status={today.getDate() > guess.day ? "out":"in"}/> )}
+          {sortedGuesses.map((guess, i) => <Guess key={i} name={guess.name} month={guess.month} day={guess.day} status={guess.status}/> )}
         </div>
         <Footer />
       </div>
