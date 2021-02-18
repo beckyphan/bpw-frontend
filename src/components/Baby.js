2aa8d481dd3101cfcecec7bba3598a1494db66d4
Becky Phan <beckyphan@live.com>
2021-02-18 13:30:08 -0800

    added alt prop to image

--- src/components/Baby.js
+++ src/components/Baby.js
@@ -6,7 +6,7 @@ function Baby() {
   return(
     <div className="App">
       <header className="App-header">
-        <img src={IMG_3822} />
+        <img src={IMG_3822} alt="baby phan-wheeler"/>
         <p>
           <Link to="/birthday" className="Hlink"><button className="soon">Birthday Guess Winners</button></Link>
         </p>
